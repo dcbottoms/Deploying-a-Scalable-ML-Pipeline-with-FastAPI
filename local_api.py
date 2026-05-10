f068c2fcd385687f43e7133dec09dcddcb4997d5
David Bottoms <dbotto1@wgu.edu>
2026-05-10 20:01:36 +0000

    Add local API request script

--- local_api.py
+++ local_api.py
@@ -1,38 +1,28 @@
-import json
-
 import requests
-
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
-
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
-
 
 
 data = {
-    "age": 37,
-    "workclass": "Private",
-    "fnlgt": 178356,
-    "education": "HS-grad",
-    "education-num": 10,
-    "marital-status": "Married-civ-spouse",
-    "occupation": "Prof-specialty",
-    "relationship": "Husband",
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
     "race": "White",
     "sex": "Male",
-    "capital-gain": 0,
+    "capital-gain": 2174,
     "capital-loss": 0,
     "hours-per-week": 40,
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+response = requests.post(
+    "http://127.0.0.1:8000/data/",
+    json=data,
+    timeout=10,
+)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+print(f"Status code: {response.status_code}")
+print(f"Response body: {response.json()}")
